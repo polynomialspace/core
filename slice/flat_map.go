16f64f64ede44f0277a5813a44f625339695e8d5
polynomialspace <github@polynomialspace>
2022-03-05 02:07:30 -0800

    add necessary type parameter

--- slice/flat_map.go
+++ slice/flat_map.go
@@ -1,7 +1,7 @@
 package iter
 
 
-func FlatMap[T any](slc []T, fn func(t T) []U) []U {
+func FlatMap[T any, U any](slc []T, fn func(t T) []U) []U {
 	ret := []U{}
 	for i, val := range slc {
 		ret = append(ret, fn(val)...)
